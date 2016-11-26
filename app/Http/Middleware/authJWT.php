1ebfa7d8d1a51ee119e76530144edcc545a83252
Luis <luis92pe>
2016-11-26 19:23:23 -0400

    CONFIGURACION MIDDLE

--- app/Http/Middleware/authJWT.php
+++ app/Http/Middleware/authJWT.php
@@ -19,17 +19,18 @@ class authJWT
      */
     public function handle($request, Closure $next)
     {
-        //  try {
+        try {
             $user = JWTAuth::toUser($request->input('token'));
-        // } catch (Exception $e) {
-        //     if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-        //         return response()->json(['error'=>'Token is Invalid']);
-        //     }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-        //         return response()->json(['error'=>'Token is Expired']);
-        //     }else{
-        //         return response()->json(['error'=>'Something is wrong']);
-        //     }
-        // }
+        } catch (Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                return response()->json(['cod' => 'WS003'  , 'msg'=>'Token is Invalid']);
+            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                return response()->json(['cod' => 'WS002'  , 'msg'=>'Token is Expired']);
+            }else{
+                return response()->json(['cod' => 'WS004'  , 'msg'=>'Something is wrong']);
+            }
+
+        }
         return $next($request);
     }
 }
