074f692b41fa172f349b78f14408764eed993e0e
Luis Peña <luis92pe@gmail.com>
2016-12-15 17:02:17 -0400

    RESPUESTA Y VALIDACIONES

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -72,29 +72,32 @@ class APIController extends Controller
          //Se obtiene el usuario
         $userAuth = Auth::user();
 
-        // dd($userAuth->co_vendedor);
+        $validateRule = [
+            'token'         => 'required',
+            'co_vendedor'   => 'required',
+            'co_sucursal'   => 'required',
+            'co_segmento'   => 'required',
+            'tipo'          => 'required',
+            'activo'        => 'required',
+            'email'         => 'required | email',
+            'descripcion'   => 'required',
+            'direccion'     => 'required',
+            'direc_entre'   => 'required',
+            'telefono'      => 'required | integer',
+            'cod_zona'      => 'required ',
+            'rif'           => 'regex:(^[JGVEP][0-9]{5,10})',
+        ];
 
-        $validator = Validator::make($request->all(), [
-            'token' => 'required',
-            'co_vendedor' => 'required',
-            'co_sucursal' => 'required',
-            'rif' => 'required | unique:clientes',
-            'co_segmento' => 'required',
-            'tipo' => 'required',
-            'activo' => 'required',
-            'email' => 'required | email',
-            'descripcion' => 'required',
-            'direccion' => 'required',
-            'direc_entre' => 'required',
-            'telefono' => 'required | integer'
-        ]);
+        //Se verifica que los parametros sean los correctos
+        $compareParameter = count(array_diff_key($request->all(), $validateRule));
+        // Validando con los requeridos y formatos
+        $validator = Validator::make($request->all(), $validateRule);
 
-
-        if ($validator->fails()) {
-            return response()->json(['cod' => 'WS003', 'msg'=>"Error con los parametros", 'validation'=>$validator->errors()]);
-        }
-
-        $user = Clientes::create([
+        if(!$compareParameter){
+            if ($validator->fails())
+                return response()->json(['cod' => 'WS003', 'msg'=>"Error con los parametros", 'validation'=>$validator->errors()]);
+            
+            $user = Clientes::create([
 				'co_vendedor'	=> $request->co_vendedor,
                 'co_zona' => $request->co_zona,
                 'rif' => $request->rif,
@@ -108,13 +111,14 @@ class APIController extends Controller
                 'telefono' => $request->telefono,
                 'created_user' => $userAuth->co_vendedor
                 
-        ]);
+            ]);
 
-        if($user){
-
-            return response()->json(['cod' => 'WS001', 'msg'=>"Cliente creado", 'validation'=>$validator->errors()]);
-        }
-            return response()->json(['cod' => 'WS002', 'msg'=>"Error al crear el cliente", 'validation'=>$validator->errors()]);
+            if($user)
+                return response()->json(['cod' => 'WS001', 'msg'=>"Cliente creado", 'validation'=>$validator->errors()]);
+            else
+                return response()->json(['cod' => 'WS002', 'msg'=>"Error al crear el cliente", 'validation'=>$validator->errors()]);
+        }else
+            return response()->json(['cod' => 'WS002', 'msg'=>"Error con cantidad de parametros", 'validation'=>$validator->errors()]);
 
     }
     
@@ -131,52 +135,57 @@ class APIController extends Controller
          //Se obtiene el usuario
         $userAuth = Auth::user();
 
-        $validator = Validator::make($request->all(), [
-            'token' => 'required',
-            'co_vendedor' => 'required',
-            'co_sucursal' => 'required',
-            'co_segmento' => 'required',
-            'tipo' => 'required',
-            'activo' => 'required',
-            'email' => 'required | email',
-            'descripcion' => 'required',
-            'direccion' => 'required',
-            'direc_entre' => 'required',
-            'telefono' => 'required | integer'
-        ]);
-
-
-        if ($validator->fails()) {
-            return response()->json(['cod' => 'WS003', 'msg'=>"Error con los parametros", 'validation'=>$validator->errors()]);
-        }
-
-        try{
-            $user = DB::table('clientes')->where('id', '=', $id)->first();
-          
-            if(!$user){
-                return response()->json(['cod' => 'WS002', 'msg'=>"Usuario no encontrado"]);
-            }
-          
-            $user->co_vendedor = $request->co_vendedor;
-            $user->co_zona = $request->co_zona;
-            $user->co_segmento = $request->co_segmento;
-            $user->tipo = $request->tipo;
-            $user->activo = $request->activo;
-            $user->email = $request->email;
-            $user->descripcion = $request->descripcion;
-            $user->direccion = $request->direccion;
-            $user->direc_entre = $request->direc_entre;
-            $user->telefono = $request->telefono;
-            $user->updated_user = $userAuth->co_vendedor;
-
-            DB::table('clientes')->where('id', '=', $id)->update(get_object_vars($user));
+        $validateRule = [
+            'token'         => 'required',
+            'co_vendedor'   => 'required',
+            'co_sucursal'   => 'required',
+            'co_segmento'   => 'required',
+            'tipo'          => 'required',
+            'activo'        => 'required',
+            'email'         => 'required | email',
+            'descripcion'   => 'required',
+            'direccion'     => 'required',
+            'direc_entre'   => 'required',
+            'telefono'      => 'required | integer'
+        ];
+        
+        //Se verifica que los parametros sean los correctos
+        $compareParameter = count(array_diff_key($request->all(), $validateRule));
+        // Validando con los requeridos y formatos
+        $validator = Validator::make($request->all(), $validateRule);
+       
+        if(!$compareParameter){
             
-            return response()->json(['cod' => 'WS001', 'msg'=>"Usuario Actualizado"]);
+            if ($validator->fails())
+                return response()->json(['cod' => 'V002', 'msg'=>"Error con los parametros", 'validation'=>$validator->errors()]);
+            
+            try{
+                $user = DB::table('clientes')->where('id', '=', $id)->first();
+            
+                if(!$user)
+                    return response()->json(['cod' => 'WS002', 'subCod' => 403, 'msg'=>"Usuario no encontrado",'data'=>null, 'validation'=> ['cod' => 'V001', 'subcod' => 200, "msg" => 'Validaciones correctas', "erros"=>$validator->errors()]]);
+            
+                $user->co_vendedor = $request->co_vendedor;
+                $user->co_zona = $request->co_zona;
+                $user->co_segmento = $request->co_segmento;
+                $user->tipo = $request->tipo;
+                $user->activo = $request->activo;
+                $user->email = $request->email;
+                $user->descripcion = $request->descripcion;
+                $user->direccion = $request->direccion;
+                $user->direc_entre = $request->direc_entre;
+                $user->telefono = $request->telefono;
+                $user->updated_user = $userAuth->co_vendedor;
 
-        }catch(\Exception $e){
-            return response()->json(['cod' => 'WS002', 'msg'=>"No se puede actualizar el usuario"]);
+                //Se graba el cliente.
+                DB::table('clientes')->where('id', '=', $id)->update(get_object_vars($user));
 
-        }
+                return response()->json(['cod' => 'WS001', 'subCod' => 200, 'msg'=>"Cliente actualizado",'data'=>null, 'validation'=> ['cod' => 'V001', 'subcod' => 200, "msg" => 'Validaciones correctas', "erros"=>$validator->errors()]]);
+            }catch(\Exception $e){
+                return response()->json(['cod' => 'WS003', 'subCod' => 500, 'msg'=>"Error Inesperado",'data'=>null, 'validation'=> ['cod' => 'V001', 'subcod' => 200, "msg" => 'Validaciones correctas', "erros"=>$validator->errors()]]);
+            }
+        }else
+            return response()->json(['cod' => 'WS002', 'msg'=>"Error con la cantidad de parametros", 'validation'=>$validator->errors()]);
     }
 
     /**
@@ -188,33 +197,38 @@ class APIController extends Controller
     public function delete(Request $request, $id)
     {
         
-      
+        $validateRule = ['token' => 'required'];
+        //Se verifica que los parametros sean los correctos
+        $compareParameter = count(array_diff_key($request->all(), $validateRule));
+        // Validando con los requeridos y formatos
+        $validator = Validator::make($request->all(), $validateRule);
+
+        if(!$compareParameter){
+            if ($validator->fails())
+                return response()->json(['cod' => 'WS003', 'msg'=>"Error con los parametros", 'validation'=>$validator->errors()]);
+
+            try{
+                $user = DB::table('clientes')->where('id', '=', $id)->first();
+                if($user){
+                    $user->activo = false; // Se cambia el estatus a 0 de inactivo
+                    DB::table('clientes')->where('id', '=', $id)->update(get_object_vars($user));
+                    // DB::table('clientes')->where('id', '=', $id)->delete();
+                    return response()->json(['cod' => 'WS001', 'msg'=>"Cliente Desactivado"]);
+                }else{
+                    return response()->json(['cod' => 'WS002', 'msg'=>"Cliente no encontrado"]);
+                }
+            }catch(\Exception $e){
+                return response()->json(['cod' => 'WS002', 'msg'=>"No se puede actualizar el cliente"]);
+            }
+        }else
+            return response()->json(['cod' => 'WS002', 'msg'=>"Error con la cantidad de parametros", 'validation'=>$validator->errors()]);
+
         $validator = Validator::make($request->all(), [
             'token' => 'required'
         ]);
 
 
-        if ($validator->fails()) {
-            return response()->json(['cod' => 'WS003', 'msg'=>"Error con los parametros", 'validation'=>$validator->errors()]);
-        }
-
-        // try{
-            $user = Clientes::find($id);
-
-            if($user){
-
-            }else{
-                return response()->json(['cod' => 'WS002', 'msg'=>"Cliente no encontrado"]);
-            }
-
-            dd($user);
-
-            $user->activo = 0; // Se cambia el estatus a 0 de inactivo
-            $user->delete();
-            return response()->json(['cod' => 'WS001', 'msg'=>"Usuario inactivo"]);
-        // }catch(\Exception $e){
-        //     return response()->json(['cod' => 'WS002', 'msg'=>"No se puede actualizar el usuario"]);
-        // }
+        
         
 
 
